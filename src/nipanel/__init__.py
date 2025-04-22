166774db1167d0db6426baca42f4db45150bee35
Mike Prosser <Mike.Prosser@emerson.com>
2025-04-22 14:38:54 -0500

    make Panel public also, for type hints

--- src/nipanel/__init__.py
+++ src/nipanel/__init__.py
@@ -1,8 +1,10 @@
 """The NI Panel."""
 
+from nipanel._panel import Panel
 from nipanel._streamlit_panel import StreamlitPanel
 
-__all__ = ["StreamlitPanel"]
+__all__ = ["Panel", "StreamlitPanel"]
 
 # Hide that it was defined in a helper file
+Panel.__module__ = __name__
 StreamlitPanel.__module__ = __name__
