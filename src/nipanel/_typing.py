8d11de5f7dcf3cb360d678c4344f6f63cb2cc675
Mike Prosser <Mike.Prosser@emerson.com>
2025-05-02 13:03:22 -0500

    better ParamSpec

--- src/nipanel/_typing.py
+++ src/nipanel/_typing.py
@@ -12,7 +12,9 @@ elif TYPE_CHECKING:
 else:
     Self = None
 
-if TYPE_CHECKING:
+if sys.version_info >= (3, 10):
+    from typing import ParamSpec
+elif TYPE_CHECKING:
     from typing_extensions import ParamSpec
 else:
     from typing import TypeVar as ParamSpec
