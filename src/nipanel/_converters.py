6457e4196430133c49bc1ca963f62fb2a1b75d85
Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>
2025-05-15 16:57:26 -0500

    Remove redundant namespacing and front-load building the type conversion dicts
    Signed-off-by: Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>

--- src/nipanel/_converters.py
+++ src/nipanel/_converters.py
@@ -5,8 +5,7 @@ from __future__ import annotations
 import logging
 from typing import Callable, NamedTuple, Union
 
-import google.protobuf.any_pb2
-from google.protobuf import wrappers_pb2
+from google.protobuf import any_pb2, wrappers_pb2
 from typing_extensions import TypeAlias
 
 _logger = logging.getLogger(__name__)
@@ -37,70 +36,73 @@ class ConvertibleType(NamedTuple):
 _CONVERTIBLE_TYPES = [
     ConvertibleType(
         python_typename=bool.__name__,
-        protobuf_typename=google.protobuf.wrappers_pb2.BoolValue.DESCRIPTOR.full_name,
-        protobuf_initializer=google.protobuf.wrappers_pb2.BoolValue,
+        protobuf_typename=wrappers_pb2.BoolValue.DESCRIPTOR.full_name,
+        protobuf_initializer=wrappers_pb2.BoolValue,
     ),
     ConvertibleType(
         python_typename=bytes.__name__,
-        protobuf_typename=google.protobuf.wrappers_pb2.BytesValue.DESCRIPTOR.full_name,
-        protobuf_initializer=google.protobuf.wrappers_pb2.BytesValue,
+        protobuf_typename=wrappers_pb2.BytesValue.DESCRIPTOR.full_name,
+        protobuf_initializer=wrappers_pb2.BytesValue,
     ),
     ConvertibleType(
         python_typename=float.__name__,
-        protobuf_typename=google.protobuf.wrappers_pb2.DoubleValue.DESCRIPTOR.full_name,
-        protobuf_initializer=google.protobuf.wrappers_pb2.DoubleValue,
+        protobuf_typename=wrappers_pb2.DoubleValue.DESCRIPTOR.full_name,
+        protobuf_initializer=wrappers_pb2.DoubleValue,
     ),
     ConvertibleType(
         python_typename=int.__name__,
-        protobuf_typename=google.protobuf.wrappers_pb2.Int64Value.DESCRIPTOR.full_name,
-        protobuf_initializer=google.protobuf.wrappers_pb2.Int64Value,
+        protobuf_typename=wrappers_pb2.Int64Value.DESCRIPTOR.full_name,
+        protobuf_initializer=wrappers_pb2.Int64Value,
     ),
     ConvertibleType(
         python_typename=str.__name__,
-        protobuf_typename=google.protobuf.wrappers_pb2.StringValue.DESCRIPTOR.full_name,
-        protobuf_initializer=google.protobuf.wrappers_pb2.StringValue,
+        protobuf_typename=wrappers_pb2.StringValue.DESCRIPTOR.full_name,
+        protobuf_initializer=wrappers_pb2.StringValue,
     ),
 ]
 
+_PROTOBUF_WRAPPER_FOR_PYTHON_TYPE = {
+    entry.python_typename: entry.protobuf_initializer for entry in _CONVERTIBLE_TYPES
+}
 
-def to_any(python_value: object) -> google.protobuf.any_pb2.Any:
+_PROTOBUF_WRAPPER_FOR_GRPC_TYPE = {
+    entry.protobuf_typename: entry.protobuf_initializer for entry in _CONVERTIBLE_TYPES
+}
+
+_SUPPORTED_PYTHON_TYPES = _PROTOBUF_WRAPPER_FOR_PYTHON_TYPE.keys()
+
+
+def to_any(python_value: object) -> any_pb2.Any:
     """Convert a Python object to a protobuf Any."""
-    protobuf_wrapper_for_type = {
-        entry.python_typename: entry.protobuf_initializer for entry in _CONVERTIBLE_TYPES
-    }
-    supported_types = set(protobuf_wrapper_for_type.keys())
     underlying_parents = [
         parent.__name__
         for parent in type(python_value).mro()  # This covers enum.IntEnum and similar
     ]
 
     best_matching_type = next(
-        (parent for parent in underlying_parents if parent in supported_types), None
+        (parent for parent in underlying_parents if parent in _SUPPORTED_PYTHON_TYPES), None
     )
     if not best_matching_type:
         raise TypeError(
-            f"Unsupported type: {type(python_value)} with parents {underlying_parents}. Supported types are: {supported_types}"
+            f"Unsupported type: {type(python_value)} with parents {underlying_parents}. Supported types are: {_SUPPORTED_PYTHON_TYPES}"
         )
     _logger.debug(f"Best matching type for '{repr(python_value)}' resolved to {best_matching_type}")
 
-    the_any = google.protobuf.any_pb2.Any()
-    wrapped_value = protobuf_wrapper_for_type[best_matching_type](value=python_value)
+    the_any = any_pb2.Any()
+    wrapped_value = _PROTOBUF_WRAPPER_FOR_PYTHON_TYPE[best_matching_type](value=python_value)
     the_any.Pack(wrapped_value)
     return the_any
 
 
-def from_any(protobuf_any: google.protobuf.any_pb2.Any) -> object:
+def from_any(protobuf_any: any_pb2.Any) -> object:
     """Convert a protobuf Any to a Python object."""
-    protobuf_wrapper_for_type = {
-        entry.protobuf_typename: entry.protobuf_initializer for entry in _CONVERTIBLE_TYPES
-    }
-    if not isinstance(protobuf_any, google.protobuf.any_pb2.Any):
+    if not isinstance(protobuf_any, any_pb2.Any):
         raise ValueError(f"Unexpected type: {type(protobuf_any)}")
 
     underlying_typename = protobuf_any.TypeName()
     _logger.debug(f"Unpacking type '{underlying_typename}'")
 
-    protobuf_wrapper = protobuf_wrapper_for_type[underlying_typename]()
+    protobuf_wrapper = _PROTOBUF_WRAPPER_FOR_GRPC_TYPE[underlying_typename]()
     did_unpack = protobuf_any.Unpack(protobuf_wrapper)
     if not did_unpack:
         raise ValueError(f"Failed to unpack Any with underlying type '{underlying_typename}'")
