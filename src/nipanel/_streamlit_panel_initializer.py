5699b8c498ce847beb054e3f1dc9744e898bbf2c
Mike Prosser <107578831+mikeprosserni@users.noreply.github.com>
2025-06-13 16:17:08 -0500

    Add initialize_panel() so that StreamlitPanelValueAccessor can be cached between runs (#70)
    * add debugpy, and initialize_panel()
    
    * panel_id = st.get_option("server.baseUrlPath").split("/")[-1]
    
    * document the debugging process in CONTRIBUTING.md
    
    * Johann's feedback
    
    ---------
    
    Co-authored-by: Mike Prosser <Mike.Prosser@emerson.com>

---
+++ src/nipanel/_streamlit_panel_initializer.py
@@ -0,0 +1,37 @@
+from typing import cast
+
+import streamlit as st
+
+from nipanel._streamlit_panel_value_accessor import StreamlitPanelValueAccessor
+
+PANEL_ACCESSOR_KEY = "StreamlitPanelValueAccessor"
+
+
+def initialize_panel() -> StreamlitPanelValueAccessor:
+    """Initialize and return the Streamlit panel value accessor.
+
+    This function retrieves the Streamlit panel value accessor for the current Streamlit script.
+    It is typically used within a Streamlit script to access and manipulate panel values.
+    The accessor will be cached in the Streamlit session state to ensure that it is reused across
+    reruns of the script.
+
+    Returns:
+        A StreamlitPanelValueAccessor instance for the current panel.
+    """
+    if PANEL_ACCESSOR_KEY not in st.session_state:
+        st.session_state[PANEL_ACCESSOR_KEY] = _initialize_panel_from_base_path()
+
+    panel = cast(StreamlitPanelValueAccessor, st.session_state[PANEL_ACCESSOR_KEY])
+    # TODO: declare the refresh component here
+    return panel
+
+
+def _initialize_panel_from_base_path() -> StreamlitPanelValueAccessor:
+    """Validate and parse the Streamlit base URL path and return a StreamlitPanelValueAccessor."""
+    base_url_path = st.get_option("server.baseUrlPath")
+    if not base_url_path.startswith("/"):
+        raise ValueError("Invalid or missing Streamlit server.baseUrlPath option.")
+    panel_id = base_url_path.split("/")[-1]
+    if not panel_id:
+        raise ValueError(f"Panel ID is empty in baseUrlPath: '{base_url_path}'")
+    return StreamlitPanelValueAccessor(panel_id)
