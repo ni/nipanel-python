8b4220605dec2345a961847d63722ee18aa80dc8
Mike Prosser <Mike.Prosser@emerson.com>
2025-06-16 13:13:48 -0500

    revert sample_panel

--- examples/sample/sample_panel.py
+++ examples/sample/sample_panel.py
@@ -8,10 +8,9 @@ panel = nipanel.initialize_panel()
 
 st.title("Sample Panel")
 
-col1, col2, col3 = st.columns([0.4, 0.3, 0.3])
+col1, col2 = st.columns([0.4, 0.6])
 
 with col1:
-    st.markdown("**Type**")
     st.write("String")
     st.write("Integer")
     st.write("Float")
@@ -19,17 +18,8 @@ with col1:
     st.write("List")
 
 with col2:
-    st.markdown("**Set Values**")
-    st.write(panel.get_value("sample_string"))
-    st.write(panel.get_value("sample_int"))
-    st.write(panel.get_value("sample_float"))
-    st.write(panel.get_value("sample_bool"))
-    st.write(panel.get_value("float_values"))
-
-with col3:
-    st.markdown("**Default Values**")
-    st.write(panel.get_value("missing_string", "0"))
-    st.write(panel.get_value("missing_int", 0))
-    st.write(panel.get_value("missing_float", 0.0))
-    st.write(panel.get_value("missing_bool", False))
-    st.write(panel.get_value("missing_list", [0.0]))
+    st.write(panel.get_value("sample_string", ""))
+    st.write(panel.get_value("sample_int", 0))
+    st.write(panel.get_value("sample_float", 0.0))
+    st.write(panel.get_value("sample_bool", False))
+    st.write(panel.get_value("float_values", []))
