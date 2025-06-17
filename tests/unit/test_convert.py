2e5f07901eed3e8713d2f80123c3aa136d517b0d
Michael Johansen <michael.johansen@ni.com>
2025-06-17 16:38:11 -0500

    Got two tests working.
    Signed-off-by: Michael Johansen <michael.johansen@ni.com>

--- tests/unit/test_convert.py
+++ tests/unit/test_convert.py
@@ -4,7 +4,6 @@ from nitypes.scalar import Scalar
 
 import nipanel._convert
 from google.protobuf import any_pb2, wrappers_pb2
-from nipanel.converters.protobuf_types import ScalarConverter
 
 
 # ========================================================
@@ -14,8 +13,26 @@ def test___bool___to_any___valid_bool_value() -> None:
     expected_bool = True
 
     result = nipanel._convert.to_any(expected_bool)
+    unpack_dest = wrappers_pb2.BoolValue(value=False)
+    _assert_any_and_unpack(result, unpack_dest)
 
-    assert isinstance(result, any_pb2.Any)
-    any_proto = any_pb2.Any()
-    assert isinstance(result, wrappers_pb2.BoolValue)
-    assert result.
+    assert isinstance(unpack_dest, wrappers_pb2.BoolValue)
+    assert unpack_dest.value == expected_bool
+
+
+def test___int___to_any___valid_int_value() -> None:
+    expected_int = 123
+
+    result = nipanel._convert.to_any(expected_int)
+    unpack_dest = wrappers_pb2.Int64Value(value=0)
+    _assert_any_and_unpack(result, unpack_dest)
+
+    assert isinstance(unpack_dest, wrappers_pb2.Int64Value)
+    assert unpack_dest.value == expected_int
+
+
+def _assert_any_and_unpack(packed_message, unpack_destination):
+    assert isinstance(packed_message, any_pb2.Any)
+    did_unpack = packed_message.Unpack(unpack_destination)
+    if not did_unpack:
+        raise ValueError(f"Failed to unpack Any with type '{packed_message.TypeName()}'")
