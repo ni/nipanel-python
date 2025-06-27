b3c955e200dba153fba0e1ba7fc9d5d43683d5ac
Mike Prosser <Mike.Prosser@emerson.com>
2025-06-27 13:52:04 -0500

    add checks to _get_python_path()

--- src/nipanel/_panel.py
+++ src/nipanel/_panel.py
@@ -37,7 +37,7 @@ class Panel(PanelValueAccessor, ABC):
             grpc_channel=grpc_channel,
         )
         self._panel_script_path = panel_script_path
-        python_path = str(Path(sys.executable).resolve())
+        python_path = self._get_python_path()
         self._panel_url = self._panel_client.start_panel(panel_id, panel_script_path, python_path)
 
     @property
@@ -49,3 +49,12 @@ class Panel(PanelValueAccessor, ABC):
     def panel_url(self) -> str:
         """Read-only accessor for the panel URL."""
         return self._panel_url
+
+    def _get_python_path(self) -> str:
+        """Get the Python path for the panel."""
+        if getattr(sys, "frozen", False):
+            raise RuntimeError("Panel cannot be used in a frozen application (e.g., PyInstaller).")
+        python_path = str(Path(sys.executable).resolve())
+        if python_path is None or python_path == "":
+            raise RuntimeError("Python environment not found")
+        return python_path
