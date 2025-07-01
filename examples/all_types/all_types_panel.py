5c64b0f2b7efc21de443ebcee2b0cf866c5df5c3
Mike Prosser <Mike.Prosser@emerson.com>
2025-07-01 15:28:59 -0500

    fix mypy

--- examples/all_types/all_types_panel.py
+++ examples/all_types/all_types_panel.py
@@ -1,6 +1,7 @@
 """A Streamlit visualization panel for the all_types.py example script."""
 
 from enum import Enum
+from typing import cast
 
 import streamlit as st
 from define_types import all_types_with_values
@@ -23,15 +24,15 @@ for name in all_types_with_values.keys():
 
     with col2:
         if isinstance(all_types_with_values[name], Enum):
-            nipanel.enum_selectbox(panel, label=name, value=default_value, key=name)
+            nipanel.enum_selectbox(panel, label=name, value=cast(Enum, default_value), key=name)
         elif isinstance(all_types_with_values[name], bool):
-            st.checkbox(label=name, value=default_value, key=name)
+            st.checkbox(label=name, value=cast(bool, default_value), key=name)
         elif isinstance(all_types_with_values[name], int):
-            st.number_input(label=name, value=default_value, key=name)
+            st.number_input(label=name, value=cast(int, default_value), key=name)
         elif isinstance(all_types_with_values[name], float):
-            st.number_input(label=name, value=default_value, key=name, format="%.2f")
+            st.number_input(label=name, value=cast(float, default_value), key=name, format="%.2f")
         elif isinstance(all_types_with_values[name], str):
-            st.text_input(label=name, value=default_value, key=name)
+            st.text_input(label=name, value=cast(str, default_value), key=name)
 
     with col3:
         st.write(panel.get_value(name))
