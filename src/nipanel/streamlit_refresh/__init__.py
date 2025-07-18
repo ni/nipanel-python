0f41a19f716440dc61b4d046fd736446d87c0eb9
Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>
2025-07-18 12:56:51 -0700

    Rework service resolution to use a global
    Signed-off-by: Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>

--- src/nipanel/streamlit_refresh/__init__.py
+++ src/nipanel/streamlit_refresh/__init__.py
@@ -2,34 +2,18 @@
 
 from __future__ import annotations
 
-from typing import ClassVar
-
 from ni_measurement_plugin_sdk_service.discovery import DiscoveryClient
 from ni_measurement_plugin_sdk_service.grpc.channelpool import GrpcChannelPool
 from streamlit.components.v1 import declare_component
 from streamlit.components.v1.custom_component import CustomComponent
 
 
-class _ServiceResolver:
-
-    _panel_service_proxy_location: ClassVar[str | None] = None
-
-    @classmethod
-    def get_or_resolve_proxy(cls) -> str:
-        if cls._panel_service_proxy_location is None:
-            discovery_client = DiscoveryClient(grpc_channel_pool=GrpcChannelPool())
-            service_location = discovery_client.resolve_service(
-                provided_interface="ni.http1.proxy",
-                service_class="",
-            )
-            cls._panel_service_proxy_location = service_location.insecure_address
-
-        return cls._panel_service_proxy_location
+_panel_service_proxy_location: str | None = None
 
 
 def initialize_refresh_component(panel_id: str) -> CustomComponent:
     """Initialize a refresh component to the Streamlit app."""
-    proxy_base_address = _ServiceResolver.get_or_resolve_proxy()
+    proxy_base_address = _get_or_resolve_proxy()
     component_url = f"http://{proxy_base_address}/panel-service/refresh/{panel_id}"
     _refresh_component_func = declare_component(
         "panelRefreshComponent",
@@ -37,3 +21,16 @@ def initialize_refresh_component(panel_id: str) -> CustomComponent:
     )
 
     return _refresh_component_func
+
+
+def _get_or_resolve_proxy() -> str:
+    global _panel_service_proxy_location
+    if _panel_service_proxy_location is None:
+        discovery_client = DiscoveryClient(grpc_channel_pool=GrpcChannelPool())
+        service_location = discovery_client.resolve_service(
+            provided_interface="ni.http1.proxy",
+            service_class="",
+        )
+        _panel_service_proxy_location = service_location.insecure_address
+
+    return _panel_service_proxy_location
