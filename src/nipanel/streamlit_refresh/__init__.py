88c7216e8fc47dfe809ec73b9cb1d9940f70e135
Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>
2025-07-18 11:19:13 -0700

    Resolve the proxy separately for refresh component
    Signed-off-by: Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>

--- src/nipanel/streamlit_refresh/__init__.py
+++ src/nipanel/streamlit_refresh/__init__.py
@@ -1,15 +1,37 @@
 """Initializes a refresh component for Streamlit."""
 
+from typing import ClassVar
+
+from ni_measurement_plugin_sdk_service.discovery import DiscoveryClient
+from ni_measurement_plugin_sdk_service.grpc.channelpool import GrpcChannelPool
 from streamlit.components.v1 import declare_component
 from streamlit.components.v1.custom_component import CustomComponent
 
 
+class _ServiceResolver:
+
+    _panel_service_proxy_location: ClassVar[str | None] = None
+
+    @classmethod
+    def get_or_resolve_proxy(cls) -> str:
+        if cls._panel_service_proxy_location is None:
+            discovery_client = DiscoveryClient(grpc_channel_pool=GrpcChannelPool())
+            service_location = discovery_client.resolve_service(
+                provided_interface="ni.http1.proxy",
+                service_class="",
+            )
+            cls._panel_service_proxy_location = service_location.insecure_address
+
+        return cls._panel_service_proxy_location
+
+
 def initialize_refresh_component(panel_id: str) -> CustomComponent:
     """Initialize a refresh component to the Streamlit app."""
-    proxy_url = "TODO"
+    proxy_base_address = _ServiceResolver.get_or_resolve_proxy()
+    component_url = f"http://{proxy_base_address}/panel-service/refresh/{panel_id}"
     _refresh_component_func = declare_component(
         "panelRefreshComponent",
-        url=f"http://{proxy_url}/panel-service/refresh/{panel_id}",
+        url=component_url,
     )
 
     return _refresh_component_func
