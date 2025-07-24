9d2f3921bdc4f5f8c62c78c0ea28aabf3088231d
Dilmi Wickramanayake <dilmi.wickramanayake@emerson.com>
2025-07-24 15:01:02 -0500

    fixing mypy error

--- examples/nidaqmx/nidaqmx_analog_input_filtering/nidaqmx_analog_input_filtering_panel.py
+++ examples/nidaqmx/nidaqmx_analog_input_filtering/nidaqmx_analog_input_filtering_panel.py
@@ -45,7 +45,7 @@ st.markdown(
 )
 
 
-def hide_run_button():
+def hide_run_button()-> None:
     """hide_run_button is used to disable run button when DAQ error pops up."""
     st.markdown(
         """
