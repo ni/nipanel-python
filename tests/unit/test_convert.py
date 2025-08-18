c42764f34e039eb308f7b9f2d2e1dedf7b0b7268
Michael Johansen <michael.johansen@ni.com>
2025-08-18 15:57:41 -0500

    Add a few missing test cases. Rename tests for consistency.
    Signed-off-by: Michael Johansen <michael.johansen@ni.com>

--- tests/unit/test_convert.py
+++ tests/unit/test_convert.py
@@ -14,7 +14,6 @@ from typing_extensions import TypeAlias
 import nipanel._convert
 import tests.types
 
-
 _AnyWrappersPb2: TypeAlias = Union[
     wrappers_pb2.BoolValue,
     wrappers_pb2.BytesValue,
@@ -130,6 +129,8 @@ _AnyPanelPbTypes: TypeAlias = Union[
             ComplexWaveform(0, ComplexInt32DType),
             "nitypes.waveform.ComplexWaveform[[('real', '<i2'), ('imag', '<i2')]]",
         ),
+        (DigitalWaveform(10, 2, np.bool, False), "nitypes.waveform.DigitalWaveform"),
+        (Spectrum(10, np.float64), "nitypes.waveform.Spectrum"),
     ],
 )
 def test___various_python_objects___get_best_matching_type___returns_correct_type_string(
@@ -251,7 +252,7 @@ def test___durationpb2_timestamp___from_any___valid_python_value() -> None:
         (array_pb2.StringArray, ["", "", ""], ["a", "b", "c"]),
     ],
 )
-def test___python_panel_collection___to_any___valid_paneltype_value(
+def test___python_collection___to_any___valid_array_proto(
     proto_type: type[_AnyPanelPbTypes], default_value: Any, expected_value: Any
 ) -> None:
     result = nipanel._convert.to_any(expected_value)
@@ -262,7 +263,7 @@ def test___python_panel_collection___to_any___valid_paneltype_value(
     assert unpack_dest.values == expected_value
 
 
-def test___python_scalar_object___to_any___valid_scalar_data_value() -> None:
+def test___python_scalar_object___to_any___valid_scalar_proto() -> None:
     scalar_obj = Scalar(1.0, "amps")
     result = nipanel._convert.to_any(scalar_obj)
     unpack_dest = scalar_pb2.Scalar()
@@ -273,7 +274,7 @@ def test___python_scalar_object___to_any___valid_scalar_data_value() -> None:
     assert unpack_dest.attributes["NI_UnitDescription"].string_value == "amps"
 
 
-def test___python_float64_analog_waveform___to_any___valid_double_analog_waveform() -> None:
+def test___python_float64_analog_waveform___to_any___valid_double_analog_waveform_proto() -> None:
     wfm_obj = AnalogWaveform(3, np.float64)
     result = nipanel._convert.to_any(wfm_obj)
     unpack_dest = waveform_pb2.DoubleAnalogWaveform()
@@ -283,7 +284,7 @@ def test___python_float64_analog_waveform___to_any___valid_double_analog_wavefor
     assert list(unpack_dest.y_data) == [0.0, 0.0, 0.0]
 
 
-def test___python_int16_analog_waveform___to_any___valid_i16_analog_waveform() -> None:
+def test___python_int16_analog_waveform___to_any___valid_i16_analog_waveform_proto() -> None:
     wfm_obj = AnalogWaveform(3, np.int16)
     result = nipanel._convert.to_any(wfm_obj)
     unpack_dest = waveform_pb2.I16AnalogWaveform()
@@ -293,7 +294,7 @@ def test___python_int16_analog_waveform___to_any___valid_i16_analog_waveform() -
     assert list(unpack_dest.y_data) == [0, 0, 0]
 
 
-def test___python_float64_complex_waveform___to_any___valid_double_complex_waveform() -> None:
+def test___python_float64_complex_waveform___to_any___valid_double_complex_waveform_proto() -> None:
     wfm_obj = ComplexWaveform(2, np.complex128)
     result = nipanel._convert.to_any(wfm_obj)
     unpack_dest = waveform_pb2.DoubleComplexWaveform()
@@ -303,7 +304,7 @@ def test___python_float64_complex_waveform___to_any___valid_double_complex_wavef
     assert list(unpack_dest.y_data) == [0.0, 0.0, 0.0, 0.0]
 
 
-def test___python_int16_complex_waveform___to_any___valid_i16_complex_waveform() -> None:
+def test___python_int16_complex_waveform___to_any___valid_i16_complex_waveform_proto() -> None:
     wfm_obj = ComplexWaveform(2, ComplexInt32DType)
     result = nipanel._convert.to_any(wfm_obj)
     unpack_dest = waveform_pb2.I16ComplexWaveform()
@@ -313,7 +314,7 @@ def test___python_int16_complex_waveform___to_any___valid_i16_complex_waveform()
     assert list(unpack_dest.y_data) == [0, 0, 0, 0]
 
 
-def test___bool_digital_waveform___to_any___valid_digital_waveform_protobuf() -> None:
+def test___python_bool_digital_waveform___to_any___valid_digital_waveform_proto() -> None:
     data = np.array([[0, 1, 0], [1, 0, 1]], dtype=np.bool)
     wfm_obj = DigitalWaveform.from_lines(data, signal_count=3)
 
@@ -326,7 +327,7 @@ def test___bool_digital_waveform___to_any___valid_digital_waveform_protobuf() ->
     assert unpack_dest.signal_count == 3
 
 
-def test___uint8_digital_waveform___to_any___valid_digital_waveform_protobuf() -> None:
+def test___python_uint8_digital_waveform___to_any___valid_digital_waveform_proto() -> None:
     data = np.array([[0, 1, 3], [7, 5, 1]], dtype=np.uint8)
     wfm_obj = DigitalWaveform.from_lines(data, signal_count=3)
 
@@ -339,7 +340,7 @@ def test___uint8_digital_waveform___to_any___valid_digital_waveform_protobuf() -
     assert unpack_dest.signal_count == 3
 
 
-def test___float64_spectrum___to_any___valid_double_spectrum_protobuf() -> None:
+def test___python_float64_spectrum___to_any___valid_double_spectrum_proto() -> None:
     spectrum = Spectrum.from_array_1d(np.array([1.0, 2.0, 3.0]))
     spectrum.start_frequency = 100.0
     spectrum.frequency_increment = 10.0
@@ -424,7 +425,7 @@ def test___python_set_of_collection_of_float___to_any___valid_double2darray(
         (array_pb2.StringArray, ["a", "b", "c"]),
     ],
 )
-def test___paneltype_value___from_any___valid_python_value(
+def test___array_proto___from_any___valid_python_collection(
     proto_type: type[_AnyPanelPbTypes], expected_value: Any
 ) -> None:
     pb_value = proto_type(values=expected_value)
@@ -436,7 +437,7 @@ def test___paneltype_value___from_any___valid_python_value(
     assert result == expected_value
 
 
-def test___scalar_data___from_any___valid_python_scalar_object() -> None:
+def test___scalar_proto___from_any___valid_python_scalar() -> None:
     attrs = {"NI_UnitDescription": attribute_value_pb2.AttributeValue(string_value="amps")}
     pb_value = scalar_pb2.Scalar(attributes=attrs, double_value=1.0)
     packed_any = _pack_into_any(pb_value)
@@ -448,7 +449,7 @@ def test___scalar_data___from_any___valid_python_scalar_object() -> None:
     assert result.units == "amps"
 
 
-def test___double_analog_waveform___from_any___valid_python_float64_analog_waveform() -> None:
+def test___double_analog_waveform_proto___from_any___valid_python_float64_analog_waveform() -> None:
     pb_value = waveform_pb2.DoubleAnalogWaveform(y_data=[0.0, 0.0, 0.0])
     packed_any = _pack_into_any(pb_value)
 
@@ -459,7 +460,7 @@ def test___double_analog_waveform___from_any___valid_python_float64_analog_wavef
     assert result.dtype == np.float64
 
 
-def test___i16_analog_waveform___from_any___valid_python_int16_analog_waveform() -> None:
+def test___i16_analog_waveform_proto___from_any___valid_python_int16_analog_waveform() -> None:
     pb_value = waveform_pb2.I16AnalogWaveform(y_data=[0, 0, 0])
     packed_any = _pack_into_any(pb_value)
 
@@ -470,7 +471,9 @@ def test___i16_analog_waveform___from_any___valid_python_int16_analog_waveform()
     assert result.dtype == np.int16
 
 
-def test___double_complex_waveform___from_any___valid_python_float64_complex_waveform() -> None:
+def test___double_complex_waveform_proto___from_any___valid_python_float64_complex_waveform() -> (
+    None
+):
     pb_value = waveform_pb2.DoubleComplexWaveform(y_data=[0.0, 0.0, 0.0, 0.0])
     packed_any = _pack_into_any(pb_value)
 
@@ -481,7 +484,7 @@ def test___double_complex_waveform___from_any___valid_python_float64_complex_wav
     assert result.dtype == np.complex128
 
 
-def test___i16_complex_waveform___from_any___valid_python_int16_complex_waveform() -> None:
+def test___i16_complex_waveform_proto___from_any___valid_python_int16_complex_waveform() -> None:
     pb_value = waveform_pb2.I16ComplexWaveform(y_data=[0, 0, 0, 0])
     packed_any = _pack_into_any(pb_value)
 
@@ -492,7 +495,7 @@ def test___i16_complex_waveform___from_any___valid_python_int16_complex_waveform
     assert result.dtype == ComplexInt32DType
 
 
-def test___digital_waveform_proto___from_any___valid_bool_digital_waveform_object() -> None:
+def test___digital_waveform_proto___from_any___valid_python_bool_digital_waveform() -> None:
     data = np.array([[0, 1, 0], [1, 0, 1]], dtype=np.bool)
     pb_value = waveform_pb2.DigitalWaveform(y_data=data.tobytes(), signal_count=3)
     packed_any = _pack_into_any(pb_value)
@@ -504,7 +507,7 @@ def test___digital_waveform_proto___from_any___valid_bool_digital_waveform_objec
     assert result.signal_count == 3
 
 
-def test___digital_waveform_proto___from_any___valid_uint8_digital_waveform_object() -> None:
+def test___digital_waveform_proto___from_any___valid_python_uint8_digital_waveform() -> None:
     data = np.array([[0, 1, 0], [1, 0, 1]], dtype=np.uint8)
     pb_value = waveform_pb2.DigitalWaveform(y_data=data.tobytes(), signal_count=3)
     packed_any = _pack_into_any(pb_value)
@@ -516,7 +519,7 @@ def test___digital_waveform_proto___from_any___valid_uint8_digital_waveform_obje
     assert result.signal_count == 3
 
 
-def test___double_spectrum_proto___from_any___valid_spectrum_object() -> None:
+def test___double_spectrum_proto___from_any___valid_python_spectrum() -> None:
     pb_value = waveform_pb2.DoubleSpectrum(
         data=[1.0, 2.0, 3.0],
         start_frequency=100.0,
